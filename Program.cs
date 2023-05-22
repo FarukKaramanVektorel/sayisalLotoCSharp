3bc51fbaadccc35b191183d80d4090c0db099b7a
Faruk Karaman <134199695+FarukKaramanVektorel@users.noreply.github.com>
2023-05-22 16:13:45 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,49 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Hello World");
+		 Console.WriteLine("Tahmini sayisal loto sonuclari");
+            Random rndNumber = new Random();
+            bool oyun = true;
+            bool reNumber = false;
+            int[] loto = new int[6];
+            while(oyun){
+                for (int i = 0; i < 6; i++)
+                {
+                     reNumber = false;
+                while (reNumber == false) {
+                    int k = 0;
+                    int randomNumber= rndNumber.Next(1, 50);
+                    for (k = 0; k < 6; k++) {
+                        if (loto[k] == randomNumber) {
+                            break;
+                        }
+                    }
+                    if (k == 6) {
+                        loto[i] = randomNumber;
+                        reNumber = true;
+                    }
+                }
+            }
+            
+           
+            Array.Sort(loto);
+            Console.WriteLine("Tahmini sayisal loto sonuclari");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write(loto[i] + "-");
+            }
+            Console.WriteLine("Tekrar oynamak istermisiniz (e/h)?");
+            string evetHayir = Console.ReadLine();
+            if (evetHayir=="e") {
+                oyun = true;
+            } else if (evetHayir=="h") {
+                oyun = false;
+                Console.WriteLine("Oyun sonlandırıldı.");
+            }           
+            }
+	}
+}
